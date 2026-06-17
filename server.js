3182c0cffdb08c7631d986a296d0e262bdea207b
deisym20 <mateopena8@gmail.com>
2026-06-17 13:36:10 -0500

    guardando cambios

--- server.js
+++ server.js
@@ -30,102 +30,27 @@ app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
 
-node server.js
-postman: http://localhost:3000/reportes
-{
-    "tipo": "comunal",
-    "descripcion": "La sociedad debe trabajar en conjunto para avanzar"
-    }   
-    const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-let reportes = [];
-
-app.get('/reportes', (req, res) => {
-    res.json(reportes);
-});
-
-app.post('/reportes', (req, res) => {
-    const reporte = {
-        id: reportes.length + 1,
-        tipo: req.body.tipo,
-        descripcion: req.body.descripcion
-    };
-    reportes.push(reporte);
-    res.json(reporte);
-});
-
-app.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000');
-});
-
-npm init -y
-npm install express
-node server.js
-postman: http://localhost:3000/reportes
-New-Item server.js -ItemType File
-code server.js
-New-Item package.json -ItemType File
-code package.json
-npm install express
-node server.js
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-let reportes = [];
-
-app.get('/reportes', (req, res) => {
-    res.json(reportes);
-});
-
-app.post('/reportes', (req, res) => {
-    const reporte = {
-        id: reportes.length + 1,
-        tipo: req.body.tipo,
-        descripcion: req.body.descripcion
-    };
-    reportes.push(reporte);
-    res.json(reporte);
-});
-
-app.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000');
-});
-
-npm init -y
-npm install express
-node server.js
-postman: http://localhost:3000/reportes
-postman: http://localhost:3000/reportes
-Json:
-{
-    "tipo": "infraestructura",
-    "descripcion": "daño de luz en el pueblo"
-}
-{
-    "id": 1,
-    "tipo": "Infraestructura",
-    "descripcion": "Daño en alumbrado público"
+app.put('/reportes/:id', (req, res) => {
+  const id = req.params.id;
+  const reporte = reportes.find(reporte => reporte.id === id);
+  if (!reporte) {
+    return res.status(404).json({ mensaje: "Reporte no encontrado" });
   }
-  get: http://localhost:3000/reportes
-  [
-    {
-      "id": 1,
-      "tipo": "Infraestructura",
-      "descripcion": "Daño en alumbrado público"
-    }
-  ]
+  reporte.tipo = req.body.tipo;
+  reporte.descripcion = req.body.descripcion;
+  res.json({ mensaje: "Reporte actualizado", reporte: reporte });
+});
 
-  
+app.delete('/reportes/:id', (req, res) => {
+  const id = req.params.id;
+  const index = reportes.findIndex(reporte => reporte.id === id);
+  if (index === -1) {
+    return res.status(404).json({ mensaje: "Reporte no encontrado" });
+  }
+  reportes.splice(index, 1);
+  res.json({ mensaje: "Reporte eliminado" });
+});   
 
-    
-
-
-
-
-
-
+npm init -y
+npm install express
+node server.js
